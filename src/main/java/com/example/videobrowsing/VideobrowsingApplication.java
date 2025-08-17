42e017546cebc216dfa2bccd514f75c5586d67ae
it24100597 <it24100597@my.sliit.lk>
2025-08-17 23:16:26 +0530

    int coomit

---
+++ src/main/java/com/example/videobrowsing/VideobrowsingApplication.java
@@ -0,0 +1,13 @@
+package com.example.videobrowsing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideobrowsingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideobrowsingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/videobrowsing/VideobrowsingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videobrowsing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideobrowsingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
