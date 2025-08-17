a28a14f654c2e41636e97066b64689fa050820fa
it24100597 <it24100597@my.sliit.lk>
2025-08-17 23:23:50 +0530

    int coomit

--- src/main/java/com/example/videobrowsing/video.java
+++ src/main/java/com/example/videobrowsing/video.java
@@ -3,5 +3,6 @@ package com.example.videobrowsing;
 public class video {
     public static void main(String[] args) {
         System.out.println("lkkk");
+        System.out.println("njkguigyfiu");
     }
 }
